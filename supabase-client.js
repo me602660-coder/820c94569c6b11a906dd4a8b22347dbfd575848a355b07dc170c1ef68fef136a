30580ac6a52534258649d76662c9717d0e6ba5ee
me602660-coder <me602660@gmail.com>
2025-09-29 10:11:53 -0300

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,7 +1,7 @@
-const { createClient } = supabase
+// supabase-client.js
+import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://chevxjgosoaoqtzmuoyy.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNoZXZ4amdvc29hb3F0em11b3l5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY5MDUzMjcsImV4cCI6MjA3MjQ4MTMyN30.V85zoO_FlbJ9JMjbg_HOYOyfT-BEiC52DA594upyp9M';
 
-const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
