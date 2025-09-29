2ba644c6cf023178bc7023e4120cfe2011b16924
me602660-coder <me602660@gmail.com>
2025-09-29 13:51:44 -0300

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,5 +1,6 @@
 // supabase-client.js
-const { createClient } = window.supabase; // ou apenas use supabase.createClient()
-
 const SUPABASE_URL = 'https://chevxjgosoaoqtzmuoyy.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNoZXZ4amd
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNoZXZ4amdvc29hb3F0em11b3l5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY5MDUzMjcsImV4cCI6MjA3MjQ4MTMyN30.V85zoO_FlbJ9JMjbg_HOYOyfT-BEiC52DA594upyp9M';
+
+// Cria uma variável GLOBAL chamada `supabase`
+window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
